cccf89f5a2e3a4cb96e0ced958984dc19d909d99
zoe-nguyen-goldenowl <zoe.nguyen.goldenowl@gmail.com>
2022-06-18 00:03:20 +0700

    excercise 1

--- exercise_1/resolver.rb
+++ exercise_1/resolver.rb
@@ -1,1 +1,12 @@
 # write code here...
+
+def exercise
+  arr = [1, 2, 3, 4, 5, 6, 7, 8]
+  sum = 10
+  answer = []
+  arr.each do |a|
+    b = sum - a
+    answer << [a, b] if arr.include?(b)
+  end
+  p answer
+end
